0ad9a43b1138f010609e0b25338fe0b7126df754
Gonza <gonl@gej.com>
2023-07-10 21:07:23 +0200

    feat: Se crea nuevas carpetas y comienza el diseño de la estructura de los objetos, segun vista de la pagina oficial.

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Testimonio() {
+    return(
+        <div className="contenedo-testimonio">
+            <img 
+            className="imagen-testimonio"
+            src= {require("../imagenes/testimonio-emma.png")}
+            alt="Foto de Emma"/>
+        </div>
+    );
+    
+}
