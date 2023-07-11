12183e88fbf25ba8e15c5489cc47fc2b3e51a54f
Gonza <gonl@gej.com>
2023-07-11 10:31:44 +0200

    feat: Crea carpeta de estilos, y comienza con los mismos.

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -11,7 +11,7 @@ function Testimonio() {
             <div className="contenedor-texto-testimonio">
                 <p className="nombre-testimonio">Emma Bostian en Suecia</p>
                 <p className="cargo-testimonio">Ingeniera de Softare en Spotify</p>
-                <p className="texto-testimonio">Siempre he tenido problemas para apredner JavaScript.</p>
+                <p className="texto-testimonio">"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."</p>
             </div>
         </div>
     );
