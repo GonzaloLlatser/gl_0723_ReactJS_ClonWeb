1b1cedcf2a416b7904f70aa8270725946c01f8e1
Gonza <gonl@gej.com>
2023-07-11 10:07:21 +0200

    feat: Eliminar carpetas no utilizadas, incluir nuevas carpetas al proyecto.

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -2,12 +2,19 @@ import React from "react";
 
 function Testimonio() {
     return(
-        <div className="contenedo-testimonio">
+        <div className="contenedor-testimonio">
             <img 
             className="imagen-testimonio"
             src= {require("../imagenes/testimonio-emma.png")}
             alt="Foto de Emma"/>
+
+            <div className="contenedor-texto-testimonio">
+                <p className="nombre-testimonio">Emma Bostian en Suecia</p>
+                <p className="cargo-testimonio">Ingeniera de Softare en Spotify</p>
+                <p className="texto-testimonio">Siempre he tenido problemas para apredner JavaScript.</p>
+            </div>
         </div>
     );
-    
 }
+
+export default Testimonio;
